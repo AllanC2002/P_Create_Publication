355381f218d9ee36bbe2c1f0ce3f58f451073b4a
Allan Correa <134990755+AllanC2002@users.noreply.github.com>
2025-06-24 14:33:46 -0500

    fix: port

--- main.py
+++ main.py
@@ -37,4 +37,4 @@ def create_publication_route():
     return jsonify(response), code
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8081, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
