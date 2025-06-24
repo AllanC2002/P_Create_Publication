b6fc2ed9528152b2ca195582812c75556cae6d7a
Allan Correa <134990755+AllanC2002@users.noreply.github.com>
2025-06-24 14:46:19 -0500

    fix: correct secrets

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import requests
 
 login_data = {
-    "User_mail": "ascorread5",
+    "User_mail": "allan",
     "password": "1234"
 }
 
-login_response = requests.post("http://localhost:8080/login", json=login_data)
+login_response = requests.post("http://52.203.72.116:8080/login", json=login_data)
 if login_response.status_code != 200:
     print("Login failed:", login_response.text)
     exit()
@@ -21,13 +21,22 @@ headers = {
 base64_img = "iVBORw0KGgoAAAANSUhEU"
 
 publication_data = {
-    "Text": "Publication type producer because we try to use ebvent drive to this microservice"
+    "Text": "Publication"
     #"Multimedia": {
         #"image_base64": base64_img,
         #"content_type": "image/png"
     #}
 }
 
-response = requests.post("http://localhost:8081/create-publication", json=publication_data, headers=headers)
+response = requests.post("http://35.173.89.233:8080/create-publication", json=publication_data, headers=headers)
 print("Status:", response.status_code)
-print("Respuesta:", response.json())
+print("Response:", response.json())
+
+print("Status code:", response.status_code)
+print("Raw response text:", response.text)
+
+try:
+    data = response.json()
+    print("JSON response:", data)
+except Exception as e:
+    print("Error decoding JSON:", e)
